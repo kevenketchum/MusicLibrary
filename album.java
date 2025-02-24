5368528968acb2f3f4843733d42cb5ee084ad2a2
Keven Ketchum <166460492+kevenketchum@users.noreply.github.com>
2025-02-24 10:21:50 -0700

    Update album.java

--- album.java
+++ album.java
@@ -1,1 +1,22 @@
+package music;
 
+public class Album extends LibraryItem {
+    private final String artist;
+
+    public Album(String name, String artist) {
+        super(name);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public void printItem() {
+        System.out.println("Album: " + name + " by " + artist);
+        for (Song song : songs) {
+            System.out.println("- " + song.getName());
+        }
+    }
+}
